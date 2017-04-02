c31eb62fb7f937b4f4739cab9a8775f09b41a1fc
dmc <fukunaga.kenichi@icloud.com>
2017-04-02 21:47:30 +0900

    バグ修正

--- src/main/java/jp/niro/jimcon/tutorial/LocalDateAdapter.java
+++ src/main/java/jp/niro/jimcon/tutorial/LocalDateAdapter.java
@@ -6,10 +6,10 @@ import javax.xml.bind.annotation.adapters.XmlAdapter;
 
 public class LocalDateAdapter extends XmlAdapter<String, LocalDate> {
     public LocalDate unmarshal(String v) throws Exception {
-        return LocalDate.parse(v);
+        return DateUtil.parse(v);
     }
 
     public String marshal(LocalDate v) throws Exception {
-        return v.toString();
+        return DateUtil.format(v);
     }
 }
