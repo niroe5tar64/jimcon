833298e150a1027e402b41080a4536881484dec3
niroe5tar64 <niroe5tar64@gmail.com>
2017-04-21 18:22:54 +0900

    propertiesファイル追加

---
+++ src/main/java/jp/niro/jimcon/ui/ProductOverviewController.java
@@ -0,0 +1,17 @@
+package jp.niro.jimcon.ui;
+
+import javafx.event.ActionEvent;
+
+/**
+ * Created by niro on 2017/04/21.
+ */
+public class ProductOverviewController {
+    public void handleNewProduct(ActionEvent actionEvent) {
+    }
+
+    public void handleEditProduct(ActionEvent actionEvent) {
+    }
+
+    public void handleDeleteProduct(ActionEvent actionEvent) {
+    }
+}
--- src/main/java/jp/niro/jimcon/ui/WindowManager.java
+++ src/main/java/jp/niro/jimcon/ui/WindowManager.java
@@ -6,7 +6,14 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.nio.file.Paths;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 /**
  * Created by niro on 2017/04/17.
@@ -14,6 +21,19 @@ import java.io.IOException;
 public class WindowManager {
     private Stage primaryStage;
     private BorderPane rootLayout;
+    private File dicDir = Paths.get("C:\\Users\\niro\\pleiades\\workspace\\jimcon\\src\\main\\resource\\jp\\niro\\jimcon").toFile();
+    private URLClassLoader urlLoader;
+    private ResourceBundle resources;
+
+    public WindowManager(){
+        try{
+            urlLoader = new URLClassLoader(new URL[]{dicDir.toURI().toURL()});
+            resources = ResourceBundle.getBundle("TextName", Locale.getDefault(),urlLoader);
+            //resources "utf8", UTF8_ENCODING_CONTROL
+        }catch (MalformedURLException e){
+            e.printStackTrace();
+        }
+    }
 
     public Stage getPrimaryStage() {
         return primaryStage;
@@ -46,8 +66,8 @@ public class WindowManager {
     public void showUnitOverview() {
         try {
             // Load person overview
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(WindowManager.class.getResource("UnitOverview.fxml"));
+            URL location = WindowManager.class.getResource("UnitOverview.fxml");
+            FXMLLoader loader = new FXMLLoader(location, resources);
             AnchorPane unitOverview = loader.load();
 
             // Set unit overview into the center of root layout.
@@ -61,8 +81,6 @@ public class WindowManager {
             e.printStackTrace();
         }
     }
-
-
 
 
 }
